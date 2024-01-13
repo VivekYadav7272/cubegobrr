bb469b420d72495973195df18efda7b1733f7156
Vivek <vivekyadav7272@gmail.com>
2024-01-13 14:09:38 +0530

    changed shadow hue again because i feel like it

--- src/quad.rs
+++ src/quad.rs
@@ -41,7 +41,7 @@ impl Quad {
         // the darker the blue hue should be.
         // so, we need to scale this value from 0 to 255 in blue channel.
 
-        let shadow_blue_hue = 30.;
+        let shadow_blue_hue = 80.;
 
         let blue = (255. - shadow_blue_hue) * -shine + shadow_blue_hue;
         t.set_fill_color(Color::rgb(0., 0., blue));
