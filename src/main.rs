d6764fe4b0a5d5da61a15ac3a967cd7ae0a89909
Vivek <vivekyadav7272@gmail.com>
2024-01-13 14:14:44 +0530

    made rotation slightly more deliberate and slower, just looks cooler

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ use util::*;
 
 use crate::cube::Cube;
 
-fn serious_stuff(d: &Drawing, t: &mut Turtle, angle: f64) {
+fn serious_stuff(d: &Drawing, t: &mut Turtle, angle_x: f64, angle_y: f64) {
     let cube = Cube::new([
         [-0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5],
         [0.5, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, 0.5],
         [-0.5, -0.5, -0.5, -0.5, 0.5, 0.5, 0.5, 0.5],
     ]);
 
-    let rot_mat = rot_x(angle * consts::PI / 180.0);
-    let rot_mat = rot_mat.dot(&rot_y(angle * consts::PI / 180.0));
+    let rot_mat = rot_x(angle_x * consts::PI / 180.0);
+    let rot_mat = rot_mat.dot(&rot_y(angle_y * consts::PI / 180.0));
 
     let rotated_cube = Cube::from(rot_mat.dot(cube.as_array()));
 
@@ -44,17 +44,18 @@ fn main() {
     let mut turtle = drawing.add_turtle();
     turtle.set_speed("instant");
 
-    let mut angle = 1.5;
-
+    let mut angle_x = 1.5;
+    let mut angle_y = 0.15;
     loop {
         turtle.clear();
 
         turtle.set_pen_color(colors::BLACK);
         draw_coordinate_axes(&drawing, &mut turtle);
 
-        serious_stuff(&drawing, &mut turtle, angle);
+        serious_stuff(&drawing, &mut turtle, angle_x, angle_y);
 
-        angle = (angle + 1.5) % 360.0;
+        angle_x = (angle_x + 1.5) % 360.0;
+        angle_y = (angle_y + 0.15) % 360.0;
 
         std::thread::sleep(std::time::Duration::from_millis(15));
     }
